d8cc389818ce15c841dcbf284f68e72eda8e55ab
77culhane <55105290+77culhane@users.noreply.github.com>
2020-03-04 15:25:15 -0500

    risk
    risk

--- runplotbot/api_keys.py
+++
@@ -1,7 +0,0 @@
-# Twitter API Keys
-consumer_key = "yemJ6EeF9EQeKbVdJDkZ6V4xa"
-consumer_secret = "xVKMNHfPPc46ZrCBW3NocYWSwNpkdFXtFlL4vdy4XzLTVQg1eW"
-access_token = "1183916454749577216-uurfZujg3tk9zFj7cUtKIkZzmbgF1G"
-access_token_secret = "PS7PeNCm8b1LKSelBMknpvaDGfXVttszEu9tlLXv5Fl7e"
-weather_api_key = "c1fcc4fef57af7ddb4a17a2f437747ec"
-user = "@CulhaneIii"
